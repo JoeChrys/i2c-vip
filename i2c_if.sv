c130a46c0bbbf48270b631d7e63dea126fe76d34
JoeChrys <josephch@windowslive.com>
2023-08-10 17:19:45 +0300

    another pullup approach

--- i2c_if.sv
+++ i2c_if.sv
@@ -1,13 +1,20 @@
 
-interface i2c_if (inout sda, scl);
+interface i2c_if ();
     
     // * * * Add you specific interface logics below * * *
-    logic vdd = 'b1;
+    logic sda;
+    logic scl;
 
-    buf(pull1, strong0) (sda, vdd);
-    buf(pull1, strong0) (scl, vdd);
-    
-	
+    /* OLD LOGIC
+    //logic vdd = 'b1; // TODO maybe use supply1 instead of logic
+    //logic sda, scl;    // TODO could also try wand/triand nets, even better a tri1 (actually tri1 is not better)
+
+    // buf(pull1, strong0) (sda, vdd);
+    // buf(pull1, strong0) (scl, vdd);
+    //pullup(sda, scl);
+
+    */
+
     // * * * You can add assertion checkers bellow * * * 
     
 
