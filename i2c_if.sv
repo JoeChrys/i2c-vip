d294e114e4effa8ebd640bf27289ef1ff79fcd2f
JoeChrys <josephch@windowslive.com>
2023-08-12 20:18:30 +0300

    new improved IF impementation with basic assertions

--- i2c_if.sv
+++ i2c_if.sv
@@ -1,28 +1,23 @@
 
 interface i2c_if ();
     
-    // * * * Add you specific interface logics below * * *
-    tri1 sda;
-    tri1 scl;
+  // * * * Add you specific interface logics below * * *
+  logic sda;
+  logic scl;
+  
+  always @(sda === 1'bz) begin
+    sda = 1;
+  end
+  
+  always @(scl === 1'bz) begin
+    scl = 1;
+  end
 
-    logic uvc_sda;
-    logic uvc_scl;
-
-    assign sda = uvc_sda;
-    assign scl = uvc_scl;
-
-    /* OLD LOGIC
-    //logic vdd = 'b1; // TODO maybe use supply1 instead of logic
-    //logic sda, scl;    // TODO could also try wand/triand nets, even better a tri1 (actually tri1 is not better)
-
-    // buf(pull1, strong0) (sda, vdd);
-    // buf(pull1, strong0) (scl, vdd);
-    //pullup(sda, scl);
-
-    */
-
-    // * * * You can add assertion checkers bellow * * * 
-    
+  // * * * You can add assertion checkers bellow * * * 
+  always @(sda or scl) begin
+    assert (sda !== 1'bx);
+    assert (scl !== 1'bx);
+  end
 
 endinterface   
     
