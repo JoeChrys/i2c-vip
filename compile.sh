5f0fe3d01f6efd86a7dd4e7ef47956b09e1e1c90
JoeChrys <josephch@windowslive.com>
2024-01-22 11:08:29 +0200

    moves top.sv to src/, updated run scripts

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #xrun top.sv -timescale 1ns/1ns -sysv -access +rw -uvm -seed 1 -uvmhome CDNS-1.2 +UVM_TESTNAME=i2c_testing_test -clean -l i2c_random_test.log +DUMPNAME="i2c_random_test.vcd" +VERBOSITY=UVM_DEBUG -elaborate
-xrun top.sv -timescale 1ns/1ns -sysv -access +rw -uvm -seed 1 -uvmhome CDNS-1.2 +UVM_TESTNAME=i2c_testing_test -incdir "src/" -incdir "src/agents" -incdir "src/env" -incdir "src/tests" -clean -l i2c_random_test.log +DUMPNAME="i2c_random_test.vcd" +VERBOSITY=UVM_DEBUG -elaborate
+xrun src/top.sv -incdir "src/" -incdir "src/agents" -incdir "src/env" -incdir "src/tests" -elaborate -sysv -access +rw -uvm -uvmhome CDNS-1.2 -clean -l i2c_testing_test.log -seed 1 -timescale 1ns/1ns +UVM_TESTNAME=i2c_testing_test +DUMPNAME="i2c_testing_test.vcd" +VERBOSITY=UVM_DEBUG
--- run_tests.sh
+++ run_tests.sh
@@ -9,4 +9,5 @@
 # xrun top.sv -timescale 1ns/1ns -sysv -access +rw -uvm -seed random -uvmhome CDNS-1.2 +UVM_TESTNAME=temp_rst_test -clean -l temp_rst_test.log +DUMPNAME="temp_rst_test.vcd" +VERBOSITY=UVM_LOW
 # xrun top.sv -timescale 1ns/1ns -sysv -access +rw -uvm -seed random -uvmhome CDNS-1.2 +UVM_TESTNAME=i2c_random_test -clean -l i2c_random_test.log +DUMPNAME="i2c_random_test.vcd" +VERBOSITY=UVM_DEBUG -gui
 # xrun top.sv -timescale 1ns/1ns -sysv -access +rw -uvm -seed 1 -uvmhome CDNS-1.2 +UVM_TESTNAME=i2c_testing_test -clean -l i2c_random_test.log +DUMPNAME="i2c_random_test.vcd" +VERBOSITY=UVM_LOW -gui
-xrun top.sv -timescale 1ns/1ns -sysv -access +rw -uvm -seed 1 -uvmhome CDNS-1.2 +UVM_TESTNAME=i2c_testing_test -clean -l i2c_random_test.log +DUMPNAME=i2c_random_test.vcd +VERBOSITY=UVM_LOW -s -input restore.tcl
+# xrun top.sv -timescale 1ns/1ns -sysv -access +rw -uvm -seed 1 -uvmhome CDNS-1.2 +UVM_TESTNAME=i2c_testing_test -clean -l i2c_random_test.log +DUMPNAME=i2c_random_test.vcd +VERBOSITY=UVM_LOW -s -input restore.tcl
+xrun src/top.sv -incdir "src/" -incdir "src/agents" -incdir "src/env" -incdir "src/tests" -s -input restore.tcl -sysv -access +rw -uvm -uvmhome CDNS-1.2 -clean -l i2c_testing_test.log -seed 1 -timescale 1ns/1ns +UVM_TESTNAME=i2c_testing_test +DUMPNAME="i2c_testing_test.vcd" +VERBOSITY=UVM_LOW
